02dbc1340818f55460d25b6a23c55a3e187de247
ihy_all <ihy_all@mail.ru>
2026-02-19 19:07:30 +0300

    added multicursors

--- init.lua
+++ init.lua
@@ -315,6 +315,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>c', group = 'Multi[C]ursor', mode = { 'n', 'v' } },
       },
     },
   },
@@ -917,6 +918,70 @@ require('lazy').setup({
         },
       },
     },
+  },
+  {
+    'jake-stewart/multicursor.nvim',
+    branch = '1.0',
+    config = function()
+      local mc = require 'multicursor-nvim'
+      mc.setup()
+
+      local set = vim.keymap.set
+
+      -- Add or skip cursor above/below the main cursor.
+      set({ 'n', 'x' }, '<c-m-k>', function() mc.lineAddCursor(-1) end, { desc = 'Multicursor: Add cursor above' })
+      set({ 'n', 'x' }, '<c-m-up>', function() mc.lineAddCursor(-1) end, { desc = 'Multicursor: Add cursor above' })
+      set({ 'n', 'x' }, '<c-m-j>', function() mc.lineAddCursor(1) end, { desc = 'Multicursor: Add cursor below' })
+      set({ 'n', 'x' }, '<c-m-down>', function() mc.lineAddCursor(1) end, { desc = 'Multicursor: Add cursor below' })
+      set({ 'n', 'x' }, '<leader>ck', function() mc.lineSkipCursor(-1) end, { desc = 'Multicursor: Move current cursor up' })
+      set({ 'n', 'x' }, '<leader>c<up>', function() mc.lineSkipCursor(-1) end, { desc = 'Multicursor: Move current cursor up' })
+      set({ 'n', 'x' }, '<leader>cj', function() mc.lineSkipCursor(1) end, { desc = 'Multicursor: Move current cursor down' })
+      set({ 'n', 'x' }, '<leader>c<down>', function() mc.lineSkipCursor(1) end, { desc = 'Multicursor: Move current cursor down' })
+
+      -- Add or skip adding a new cursor by matching word/selection
+      set({ 'n', 'x' }, '<leader>cn', function() mc.matchAddCursor(1) end, { desc = 'Multicursor: Add cursor to next match' })
+      set({ 'n', 'x' }, '<leader>cs', function() mc.matchSkipCursor(1) end, { desc = 'Multicursor: Move cursor to next match' })
+      set({ 'n', 'x' }, '<leader>cN', function() mc.matchAddCursor(-1) end, { desc = 'Multicursor: Add cursor to previous match' })
+      set({ 'n', 'x' }, '<leader>cS', function() mc.matchSkipCursor(-1) end, { desc = 'Multicursor: Move cursor to previous match' })
+
+      -- Add and remove cursors with control + left click.
+      set('n', '<c-leftmouse>', mc.handleMouse)
+      set('n', '<c-leftdrag>', mc.handleMouseDrag)
+      set('n', '<c-leftrelease>', mc.handleMouseRelease)
+
+      -- Disable and enable cursors.
+      set({ 'n', 'x' }, '<c-q>', mc.toggleCursor, { desc = 'Multicursor: Toggle cursor' })
+
+      -- Mappings defined in a keymap layer only apply when there are
+      -- multiple cursors. This lets you have overlapping mappings.
+      mc.addKeymapLayer(function(layerSet)
+        -- Select a different cursor as the main one.
+        layerSet({ 'n', 'x' }, '<left>', mc.prevCursor, { desc = 'Multicursor: Make previous cursor main' })
+        layerSet({ 'n', 'x' }, '<right>', mc.nextCursor, { desc = 'Multicursor: Make next cursor main' })
+
+        -- Delete the main cursor.
+        layerSet({ 'n', 'x' }, '<leader>cx', mc.deleteCursor, { desc = 'Multicursor: Delete main cursor' })
+
+        -- Enable and clear cursors using escape.
+        layerSet('n', '<esc>', function()
+          if not mc.cursorsEnabled() then
+            mc.enableCursors()
+          else
+            mc.clearCursors()
+          end
+        end)
+      end)
+
+      -- Customize how cursors look.
+      local hl = vim.api.nvim_set_hl
+      hl(0, 'MultiCursorCursor', { reverse = true })
+      hl(0, 'MultiCursorVisual', { link = 'Visual' })
+      hl(0, 'MultiCursorSign', { link = 'SignColumn' })
+      hl(0, 'MultiCursorMatchPreview', { link = 'Search' })
+      hl(0, 'MultiCursorDisabledCursor', { reverse = true })
+      hl(0, 'MultiCursorDisabledVisual', { link = 'Visual' })
+      hl(0, 'MultiCursorDisabledSign', { link = 'SignColumn' })
+    end,
   },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
