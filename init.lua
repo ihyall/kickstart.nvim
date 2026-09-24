3dbffd4b0f16e463e048960258aa844667638868
ihy_all <ihy_all@mail.ru>
2026-09-24 14:32:22 +0300

    Added treesitter parsers for my languages

--- init.lua
+++ init.lua
@@ -170,13 +170,6 @@ vim.o.confirm = true
 -- This option controls the default look of window borders, commented because it messes with autocompletion and stuff
 -- vim.o.winborder = 'rounded'
 
--- Folding control (moved to nvim-ufo config)
--- vim.o.foldmethod = 'syntax'
--- vim.o.foldlevel = 99
--- vim.o.foldlevelstart = 99
--- vim.o.foldcolumn = '0'
--- vim.o.foldenable = true
-
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -680,14 +673,29 @@ require('lazy').setup({
 
           client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
             runtime = {
+              -- Tell the language server which version of Lua you're using (most
+              -- likely LuaJIT in the case of Neovim)
               version = 'LuaJIT',
-              path = { 'lua/?.lua', 'lua/?/init.lua' },
+              -- Tell the language server how to find Lua modules same way as Neovim
+              -- (see `:h lua-module-load`)
+              path = {
+                'lua/?.lua',
+                'lua/?/init.lua',
+              },
             },
+            -- Make the server aware of Neovim runtime files
             workspace = {
               checkThirdParty = false,
-              -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
-              --  See https://github.com/neovim/nvim-lspconfig/issues/3189
-              library = vim.api.nvim_get_runtime_file('', true),
+              library = {
+                vim.env.VIMRUNTIME,
+                -- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
+                vim.api.nvim_get_runtime_file('lua/lspconfig', false)[1],
+              },
+              -- Or pull in all of 'runtimepath'.
+              -- NOTE: this is a lot slower and will cause issues when working on
+              -- your own configuration.
+              -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+              -- library = vim.api.nvim_get_runtime_file('', true),
             },
           })
         end,
